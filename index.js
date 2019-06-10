7f931bbd2da39571777bc31fc6f08803d37bc775
elvino <elvino_tan@yahoo.com>
2019-06-10 17:17:23 +0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
+// Import a Libraray to help create component
+import React from "react";
+import { Text, View, AppRegistry } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create Component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={"Albums"} />
+      <AlbumList />
+    </View>
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+// Render it to the device
+AppRegistry.registerComponent("albums", () => App);
